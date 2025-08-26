9f96edcc6de420d2fc73d01d002f1067b18a43fa
lifaet <lifaethossain@gmail.com>
2025-08-26 18:37:17 +0600

    Data format chagne

--- resources/views/backend/orders/pos-invoice.blade.php
+++ resources/views/backend/orders/pos-invoice.blade.php
@@ -31,7 +31,7 @@
       </div>
       <div class="text-right">
         <address class="text-right">
-          <p>{{ date('d/M/Y') }}</p>
+          <p>{{ date('d-M-Y') }}</p>
           <p>{{ date('h:i:s A') }}</p>
         </address>
       </div>
