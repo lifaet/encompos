d6a88a4d88150dd74fb8e847169e0eba7787c096
lifaet <lifaethossain@gmail.com>
2025-08-31 22:41:03 +0600

    s

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
         Paginator::useBootstrap();
     }
     
