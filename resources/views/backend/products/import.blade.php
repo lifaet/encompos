6eb9f1f7b2110ce2efce558ccf4bd3c56ffa1a1a
lifaet <lifaethossain@gmail.com>
2025-10-04 19:32:38 +0600

    add buck name preview

--- resources/views/backend/products/import.blade.php
+++ resources/views/backend/products/import.blade.php
@@ -19,16 +19,18 @@
                   <label class="custom-file-label" for="exampleInputFile">Choose file</label>
                 </div>
                 <div class="input-group-append">
-                  <a class="input-group-text" href="{{ route('backend.admin.products.import',['download-demo' => true]) }}"><i class="fas fa-download"></i> Demo</a>
+                  <a class="input-group-text" href="{{ route('backend.admin.products.import',['download-demo' => true]) }}">
+                    <i class="fas fa-download"></i> Demo
+                  </a>
                 </div>
               </div>
+              <small id="selectedFileName" class="form-text text-muted mt-2"></small>
             </div>
           </div>
         </div>
         <div class="row">
           <div class="mb-3 col-md-6">
             <button type="submit" class="btn btn-block bg-gradient-primary">Save</button>
-            <!-- /.card-body -->
           </div>
         </div>
       </div>
@@ -43,8 +45,18 @@
     height: calc(1.5em + 0.75rem + 2px) !important;
   }
 </style>
-
 @endpush
 @push('script')
 <script src="{{ asset('js/image-field.js') }}"></script>
+<script>
+  document.getElementById('exampleInputFile').addEventListener('change', function () {
+    const file = this.files[0];
+    const fileNameDisplay = document.getElementById('selectedFileName');
+    if (file) {
+      fileNameDisplay.textContent = `Selected file: ${file.name}`;
+    } else {
+      fileNameDisplay.textContent = '';
+    }
+  });
+</script>
 @endpush
